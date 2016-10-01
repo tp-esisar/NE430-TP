01a47824f0f01c91028b0933ac71b96f28a182b8
Alexis Bertrand <zipper97412@gmail.com>
2016-10-01 15:02:29 +0200

    optimisation de noeud terminal

--- multibit.c
+++ multibit.c
@@ -1,13 +1,29 @@
 #include <stdint.h>
 
+#define VIDE 0
+#define NODE 1
+#define NODE_OPT 2
+typedef struct __attribute__((__packed__)) NodeOpti {
+	unsigned int padding: 24;
+	unsigned int cidr: 8;
+	unsigned int gw: 32;
+}NodeOpti;
 
+typedef struct __attribute__((__packed__)) pNode {
+	unsigned char type;
+	union {
+		struct Node* node;
+		NodeOpti nodeOpti;
+	};
+}pNode;
 
-typedef struct Node {
+typedef struct __attribute__((__packed__)) Node {
 	unsigned int gw;
 	char cidr;
-    struct Node* childs[];
-
+    pNode childs[];
 }Node;
+
+
 
 Node* tree = NULL;
 //char strides[32];
@@ -17,13 +33,19 @@ char strides[] = {16,8,4,2,2};
 void initMyAlgo() {}
 
 Node* newNode(char stride, unsigned int gw, char cidr) {
-	Node* temp = calloc((sizeof(unsigned int)+sizeof(char)+sizeof(Node*)*(1<<stride)),1);
+	Node* temp = calloc((sizeof(unsigned int)+sizeof(char)+sizeof(pNode)*(1<<stride)),1);
 	if (temp == NULL) {
 		fprintf(stderr, "erreur malloc newNode");
 		exit(1);
 	}
 	temp->gw = gw;
 	temp->cidr = cidr;
+	return temp;
+}
+NodeOpti newNodeOpti(unsigned int gw, char cidr) {
+	NodeOpti temp;
+	temp.gw = gw;
+	temp.cidr = cidr;
 	return temp;
 }
 
@@ -37,12 +59,20 @@ char getCIDR(unsigned int netmask) {
 
 void setChildsRange(Node* node, unsigned int min, unsigned int max, unsigned int gw, char cidr,char stride) {
 	for(unsigned int i = min;i<max;i++) {
-		if(node->childs[i] == NULL) {
-			node->childs[i] = newNode(stride,gw,cidr);
+		if(node->childs[i].type == VIDE) {
+			//faire l'opti de stockage
+			node->childs[i].type = NODE_OPT;
+			node->childs[i].nodeOpti = newNodeOpti(gw,cidr);
 		}
-		else if (node->childs[i]->cidr <= cidr) {
-			node->childs[i]->cidr = cidr;
-			node->childs[i]->gw = gw;
+		else if (node->childs[i].type == NODE_OPT &&
+				 node->childs[i].nodeOpti.cidr <= cidr) {
+			node->childs[i].nodeOpti.cidr = cidr;
+			node->childs[i].nodeOpti.gw = gw;
+		}
+		else if (node->childs[i].type == NODE &&
+				 node->childs[i].node->cidr <= cidr) {
+			node->childs[i].node->cidr = cidr;
+			node->childs[i].node->gw = gw;
 		}
 	}
 }
@@ -67,10 +97,16 @@ void insertMyAlgo(unsigned int addr,unsigned int netmask,unsigned int gw) {
 		}
 		else {
 			//cas intermédiaire
-			if(local->childs[index]==NULL) {
-				local->childs[index] = newNode(strides[deep+1],0,0);
+			if(local->childs[index].type==VIDE) {
+				local->childs[index].type = NODE;
+				local->childs[index].node = newNode(strides[deep+1],0,0);
 			}
-			local = local->childs[index];
+			else if (local->childs[index].type == NODE_OPT) {
+				NodeOpti temp = local->childs[index].nodeOpti;
+				local->childs[index].type = NODE;
+				local->childs[index].node = newNode(strides[deep+1],temp.gw,temp.cidr);
+			}
+			local = local->childs[index].node;
 			walk += strides[deep];
 			deep++;
 
@@ -92,11 +128,14 @@ unsigned int lookupMyAlgo(unsigned int addr) {
 		if(local->gw != 0) {
 			bestMatchGw = local->gw;
 		}
-		if(local->childs[index]==NULL) {
-			return local->gw;
+		if(local->childs[index].type==VIDE) {
+			return bestMatchGw;
+		}
+		else if (local->childs[index].type == NODE_OPT) {
+			return local->childs[index].nodeOpti.gw;
 		}
 		else {
-			local = local->childs[index];
+			local = local->childs[index].node;
 			walk += strides[deep];
 			deep++;
 		}
